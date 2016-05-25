96b53500bd6e3cb3884e9e11ee27a56ebb358615
Toni Aho <toni.aho@siili.fi>
2016-05-26 00:26:02 +0300

    FB.ui

--- src/home/card/card.component.js
+++ src/home/card/card.component.js
@@ -5,6 +5,18 @@ const Card = {
   template,
   bindings: {
     item: '<'
+  },
+  controller: function() {
+
+    this.sendMessage = function () {
+      console.log('heihei');
+      FB.ui({
+        method: 'send',
+        link: 'http://www.pesis.fi',
+        to: '527189480',
+        message: 'tuu mukaan treeneihin'
+      });
+    };
   }
 };
 export default Card;
